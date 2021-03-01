e83b054a554dd3020cabf79153a895cc3fc6ef4f
natsukawadaisuki <60532865+natsukawadaisuki@users.noreply.github.com>
2021-03-01 15:47:50 +0900

    modified

--- SearchTweetsAndMarkovChain.py
+++ SearchTweetsAndMarkovChain.py
@@ -23,8 +23,11 @@ def tweet_search(search_words):
     results = api.search(q=word, count=set_count)
     strResult = ""
     for result in results:
+        # if "RT" not in result.text and "@" not in result.text:
         if "RT" not in result.text and "@" not in result.text and "https://t.co/" not in result.text:
             strResult += result.text
+    print("-----Generate text based on this text-----")
+    print(strResult)
     return strResult
 
 # Create a dictionary by splitting text data using Janome
@@ -33,18 +36,19 @@ def wakati(text):
    text = text.replace('\r','')
    t = Tokenizer()
    result =t.tokenize(text, wakati=True)
+   print("-----Generate words list-----")
+   print(result)
    return result
 
-def generate_text(wordlist):
+def generate_text(words_list):
    num_sentence = 5
-   src = wordlist
-   wordlist = wakati(src)
+   words_list = words_list
  
    # Create a table for Markov chains 
    markov = {}
    w1 = ""
    w2 = ""
-   for word in wordlist:
+   for word in words_list:
        if w1 and w2:
            if (w1, w2) not in markov:
                markov[(w1, w2)] = []
@@ -52,7 +56,7 @@ def generate_text(wordlist):
        w1, w2 = w2, word
  
    # Automatic sentence generation
-   count_kuten = 0 # Number of punctuation marks "。"(JPN) 
+   count_kuten = 0 #  "。"
    num_sentence = num_sentence
    sentence = ""
    w1, w2  = random.choice(list(markov.keys()))
@@ -71,11 +75,14 @@ def generate_text(wordlist):
            sentence += '\n'
        w1, w2 = w2, tmp
 
+   print("-----Generated Text-----")
+   print(sentence)
+
    return sentence
     
 if __name__ == "__main__":
     # Accepts search terms as arguments
-    wordslist = []
+    search_words = []
     parser = argparse.ArgumentParser(description='search words(space separated)')
     parser.add_argument('-w','--words',metavar='words',type=str,help='search words(space separated)',default="")
     args = parser.parse_args()
@@ -85,14 +92,11 @@ if __name__ == "__main__":
         sys.exit()
     # Split space separators into comma-separated lists
     s = args.words
-    wordslist = s.split()
-    print(wordslist)
-    # Search for tweets data
-    tweet_search = tweet_search(wordslist)
-    print(tweet_search)
-    tweet_text = generate_text(tweet_search)
-    print("-----The original text of the tweet-----")
-    print(tweet_text)
+    search_words = s.split()
+    print(search_words)
+    # Search for tweets data and generate text
+    tweet_text = generate_text(wakati(tweet_search(search_words)))
+    # trim
     tweet_text_140 = tweet_text[1:140]
     print("-----Text trimmed to 140 characters-----")
     print(tweet_text_140)
@@ -100,4 +104,8 @@ if __name__ == "__main__":
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
     # tweet
-    api.update_status(tweet_text_140)
+    try:
+        api.update_status(tweet_text_140)
+    except Exception as e:
+        print(e)
+        print(type(e))
